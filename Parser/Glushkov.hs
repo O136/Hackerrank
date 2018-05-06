d8cf55a0f25d64fd0a8403e5b1dfaaeb4d8e8479
Oleg Patraschku <oleg.patraschku@gmx.de>
2018-05-06 09:29:06 +0000

    better names

--- Parser/Glushkov.hs
+++ Parser/Glushkov.hs
@@ -1,46 +1,47 @@
 module Glushkov where
-import Prelude hiding (last)
 
 --If we see epsilon in regex pattern it will be Letter (_, 'eps'), so Epsilon cons has a role of more like Nil
-data Node = Epsilon | Letter (Int, Char) | Star Node | Concat(Node, Node) | Or(Node, Node) deriving (Show, Eq)
-initial = Letter (-1, '&')
+data RegT = Epsilon | Letter (Int, Char) | Star RegT | Concat (RegT, RegT) | Or (RegT, RegT) deriving (Show, Eq)
 
-empty :: Node -> Bool
-empty e = case e of
-  Concat(l, r) -> empty l && empty r
-  Or(l, r) -> empty l || empty r
-  Letter _ -> False
-  _ -> True
+initS :: RegT
+initS = Letter (-1, '&')
 
---returns all the states that can be reached from the initial state
-first :: Node -> [Node]
-first e = case e of
-  Or(r1, r2) -> first r1 ++ first r2
-  Concat(r1, r2) -> if empty r1 then first r1 ++ first r2 else first r1
-  x@(Letter _) -> [x]
-  Star r1 -> first r1
-  Epsilon -> []
+empty :: RegT -> Bool
+empty t = case t of
+    Concat (l, r) -> empty l && empty r
+    Or (l, r) -> empty l || empty r
+    Letter _ -> False
+    _ -> True
 
---returns the next possible states for a particular leaf
-next :: Node -> Node -> [Node]
-next e leaf = next e leaf [] where
-  next e leaf acc = case e of
-    Concat(r1, r2) -> next r1 leaf r1' ++ next r2 leaf acc where
-      r1' = if empty r2 then first r2 ++ acc else first r2
-    Or(r1, r2) -> next r1 leaf acc ++ next r2 leaf acc
-    Star r1 -> next r1 leaf (first r1 ++ acc)
-    x@(Letter _) -> if leaf == x then acc else []
+--all the states that can be reached from the initS state
+firstS :: RegT -> [RegT]
+firstS t = case t of
+    Or (r1, r2) -> firstS r1 ++ firstS r2
+    Concat (r1, r2) -> if empty r1 then firstS r1 ++ firstS r2 else firstS r1
+    x@(Letter _) -> [x]
+    Star r1 -> firstS r1
     Epsilon -> []
 
---returns all the states which are accept states without the initial state
-last :: Node -> [Node]
-last e = case e of
-  Concat(r1, r2) -> if empty r2 then last r1 ++ last r2 else last r2
-  Or(r1, r2) -> last r1 ++ last r2
-  Star r1 -> last r1
-  x@(Letter _) -> [x]
-  Epsilon -> []
+--the next possible states for a particular leaf
+nextS :: RegT -> RegT -> [RegT]
+nextS t leaf = nextS t leaf [] where
+  nextS t leaf acc = case t of
+        Concat (r1, r2) -> nextS r1 leaf r1' ++ nextS r2 leaf acc where
+          r1' = if empty r2 then firstS r2 ++ acc else firstS r2
+        Or (r1, r2) -> nextS r1 leaf acc ++ nextS r2 leaf acc
+        Star r1 -> nextS r1 leaf (firstS r1 ++ acc)
+        x@(Letter _) -> if leaf == x then acc else []
+        Epsilon -> []
 
---returns all the states which are accept states, taking in account if initial state is empty
-accept :: Node -> [Node]
-accept t = if not (empty t) then last t else initial : last t
+--all the states which are accept states without the initS state
+lastS :: RegT -> [RegT]
+lastS t = case t of
+    Concat (r1, r2) -> if empty r2 then lastS r1 ++ lastS r2 else lastS r2
+    Or (r1, r2) -> lastS r1 ++ lastS r2
+    Star r1 -> lastS r1
+    x@(Letter _) -> [x]
+    Epsilon -> []
+
+--all the states which are accept states, taking in account if initS state is empty
+acceptS :: RegT -> [RegT]
+acceptS t = if not (empty t) then lastS t else initS : lastS t
--- Parser/GlushkovGraphViz.hs
+++ Parser/GlushkovGraphViz.hs
@@ -1,28 +1,33 @@
 module GlushkovGraphViz where
-import Glushkov
-import Data.Maybe (isJust)
-import Data.List (find)
 
+import Data.List (find)
+import Data.Maybe (isJust)
+import Glushkov
+
+--TODO : create a new structure for automaton
 --given a regex tree and a word, returns whether word is accepted or not
---it builds the automaton on the fly as needed
-automaton :: Node -> String -> Bool
-automaton reg word = auto' reg word [initial] where
-  auto' reg []     fin   = or $ map (\s-> isJust $ find (==s) fin) (accept reg)
-  auto' reg (l:ls) nsts = auto' reg ls nsts' where
-    nsts' = filter (\(Letter(_, l')) -> l' == l)
-               (concatMap (\s -> if s == initial then first reg
-                                 else next reg s) nsts)
+isValid :: RegT -> String -> Bool
+isValid t word = isValid' t word [initS] where
+    isValid' t [] fin      = or $ map (\s -> isJust $ find (== s) fin) (acceptS t)
+    isValid' t (l:ls) nsts = isValid' t ls nsts' where
+        nsts' = filter (\(Letter (_, l')) -> l' == l)
+                       (concatMap (\s -> if s == initS then firstS t
+                                         else nextS t s) nsts)
 
 --(a|b)*a(a|b)
-reg2 = Concat(Star(Or(Letter (0,'a'), Letter (1, 'b'))),
-             Concat(Letter (2, 'a'), Or(Letter (3, 'a'), Letter (4, 'b'))))
+regEx :: RegT
+regEx = Concat
+       (Star (Or (Letter (0, 'a'), Letter (1, 'b'))),
+        Concat (Letter (2, 'a'), Or (Letter (3, 'a'), Letter (4, 'b'))))
 
-tran2String :: (Node, [Node]) -> [String]
-tran2String (s, s') = case s of
-  Letter(i, l) -> map f s' where
-    f = \(Letter(i', l')) -> show i ++ "->" ++ show i' ++ " [label=" ++ show l' ++ "];"
-  _ -> [""]
-
+tran2String :: (RegT, [RegT]) -> [String]
+tran2String (s, s') =
+  case s of
+    Letter (i, _) -> map f s'
+      where f =
+              \(Letter (i', l')) ->
+                show i ++ "->" ++ show i' ++ " [label=" ++ show l' ++ "];"
+    _ -> [""]
 {-
 rankdir=LR;
 size="8,5"
@@ -31,4 +36,3 @@ node [shape=doublecircle];
 4;5;
 node [shape=circle];
 -}
-
