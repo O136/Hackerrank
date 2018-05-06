2271964aa2a7e126d2f0ff22982dcd135a5a7bfc
Oleg Patraschku <oleg.patraschku@gmx.de>
2018-05-06 13:49:27 +0000

    gibiansky indent style

--- Parser/GlushkovGraphViz.hs
+++ Parser/GlushkovGraphViz.hs
@@ -7,18 +7,31 @@ import Glushkov
 --TODO : create a new structure for automaton
 --given a regex tree and a word, returns whether word is accepted or not
 isValid :: RegT -> String -> Bool
-isValid t word = isValid' t word [initS] where
-    isValid' t []     fin  = or $ map (\s -> isJust $ find (== s) fin) (acceptS t)
-    isValid' t (l:ls) nsts = isValid' t ls nsts' where
-        nsts' = filter (\(Letter (_, l')) -> l' == l)
-                       (concatMap (\s -> if s == initS then firstS t
-                                         else nextS t s) nsts)
+isValid t word = isValid' t word [initS]
+  where
+    isValid' t [] fin = or $ map (\s -> isJust $ find (== s) fin) (acceptS t)
+    isValid' t (l:ls) nsts = isValid' t ls nsts'
+      where
+        nsts' =
+          filter
+            (\(Letter (_, l')) -> l' == l)
+            (concatMap
+               (\s ->
+                  if s == initS
+                    then firstS t
+                    else nextS t s)
+               nsts)
 
 --(a|b)*a(a|b)
 regEx :: RegT
-regEx = Concat
-       (Star (Or (Letter (0, 'a'), Letter (1, 'b'))),
-        Concat (Letter (2, 'a'), Or (Letter (3, 'a'), Letter (4, 'b'))))
+regEx =
+  Concat
+    ( Star (Or (Letter (0, 'a'), Letter (1, 'b'))),
+      Concat (Letter (2, 'a'), Or (Letter (3, 'a'), Letter (4, 'b'))))
+
+--(a)*
+regEx2 :: RegT
+regEx2 = Star (Letter (0, 'a'))
 
 tran2String :: (RegT, [RegT]) -> [String]
 tran2String (s, s') =
