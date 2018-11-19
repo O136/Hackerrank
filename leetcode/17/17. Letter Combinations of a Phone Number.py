4e15e0bfa24894beeeb6f889dc72873f991741be
Oleg Patraschku <oleg.patraschku@gmail.com>
2018-11-19 11:37:27 +0100

    17

---
+++ leetcode/17/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,33 @@
+class Solution:
+    def pairwise(self, l1, l2): # [[concat(i,j)] ∀i ∊ l1 and ∀j ∊ l2]
+        return [''.join([e1,e2]) for e1 in l1 for e2 in l2]
+
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if len(digits) == 0:
+            return []
+        #converting from ascii to integer
+        dRange = list(map(lambda d : ord(d) - ord('2'), digits))
+
+        d = [['a', 'b', 'c'], ['d', 'e','f'], ['g','h','i'], ['j','k','l'],
+             ['m','n','o'], ['p','q','r','s'], ['t','u','v'], ['w','x','y','z']]
+
+        #removing a random index
+        acc = d[dRange.pop(0)]
+        
+        for seq in dRange:
+            #for more readability would use the function pairwise
+            acc = [''.join([e1,e2]) for e1 in d[seq] for e2 in acc] 
+            
+        return list(acc)
+
+		
+
+
+s = Solution()
+print(s.pairwise(['a','b'],['c','d']))
+print(s.letterCombinations(""))
+print(s.letterCombinations("27"))
